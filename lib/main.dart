a1067982c4db63b4c3009faf175feeda91472cda
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2025-10-08 13:46:01 +0700

    praktikum 4

--- lib/main.dart
+++ lib/main.dart
@@ -78,8 +78,14 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(
+        body: ListView(
           children: [
+            Image.asset(
+              'images/welirang.jpeg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
             titleSection,
             buttonSection,
             textSection,
